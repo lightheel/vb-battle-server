a2a3231c5c3d2ee9d7ab2de418a267346050e208
lightheel <adrik.lightheel@gmail.com>
2025-01-19 13:25:45 -0500

    Small cleanup.
    Changed JSON response and removed unused code.
    
    Removed hardcoded base HP reference when resetting HP in between combat.

--- src/main/java/com/example/restservice/CombatController.java
+++ src/main/java/com/example/restservice/CombatController.java
@@ -1,7 +1,6 @@
 package com.example.restservice;
 
 import static java.lang.Math.round;
-import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.vb_battle_server.Character;
 import java.util.Random;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CombatController {
-    //private final AtomicLong counter = new AtomicLong();
     Random rand = new Random();
 
     //Stages
@@ -26,15 +24,13 @@ public class CombatController {
     // 2 Data
     // 3 Vaccine
     // 4 Free
-    private final Character digimon1 = new Character("GulusGammamon",1,2,3700, 3700, 5300,1250);
-    private final Character digimon2 = new Character("ShellNumemon",1,2,3000, 3000,4800,1150);
+    private final Character digimon1 = new Character("Gulus Gammamon",1,2,3700, 3700, 5300,1250);
+    private final Character digimon2 = new Character("Shell Numemon",1,2,3000, 3000,4800,1150);
 
     boolean round1 = false;
     boolean round2 = false;
     boolean round3 = false;
     boolean round4 = false;
-    String combatResults = "";
-
     boolean CombatRound(Character digi1, Character digi2){
         float hitrate = (digi1.getBaseBp() / (digi1.getBaseBp() + digi2.getBaseBp()) * 100);
         int rand_int = rand.nextInt(99);
@@ -68,13 +64,13 @@ public class CombatController {
             }
         }
         if (digi1.getCurrentHp() > digi2.getCurrentHp()){
-            digi1.setCurrentHp(3700);
-            digi2.setCurrentHp(3000);
+            digi1.setCurrentHp(digimon1.getBaseHp());
+            digi2.setCurrentHp(digimon2.getBaseHp());
             return digi1;
         }
         else {
-            digi1.setCurrentHp(3700);
-            digi2.setCurrentHp(3000);
+            digi1.setCurrentHp(digimon1.getBaseHp());
+            digi2.setCurrentHp(digimon2.getBaseHp());
             return digi2;
         }
     }
@@ -82,8 +78,6 @@ public class CombatController {
     @GetMapping("/combat")
     public Combat combat() {
         Character winner = CombatCheck(digimon1, digimon2);
-        //combatResults = String.format(template, winner.getName(), round1, round2, round3, round4);
-        combatResults = winner.getName();
-        return new Combat(combatResults, round1, round2, round3, round4);
+        return new Combat(winner.getName(), round1, round2, round3, round4);
     }
 }
